203d96b30015c5585644007565c8c41e7b412931
Raiymbek Yeslyam <yeslyamr@gmail.com>
2024-12-24 21:07:56 +0500

    feat: json helper method + healthcheck

---
+++ cmd/api/helpers.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(js)
+	if err != nil {
+		return err
+	}
+	return nil
+}
