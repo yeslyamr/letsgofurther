eb2f93647a260ab729603e0cb7f6ba07a51acba1
Raiymbek Yeslyam <yeslyamr@gmail.com>
2024-12-25 11:48:20 +0500

    feat: json error messages

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func (app *application) routes() *mux.Router {
 	router := mux.NewRouter()
+
+	router.MethodNotAllowedHandler = http.HandlerFunc(app.methodNotAllowedResponse)
+	router.NotFoundHandler = http.HandlerFunc(app.notFoundResponse)
 
 	router.HandleFunc("/v1/healthcheck", app.healthcheckHandler).Methods("GET")
 	router.HandleFunc("/v1/movies", app.createMovieHandler).Methods("POST")
