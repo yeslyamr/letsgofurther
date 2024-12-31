f73968296c8cc70c07ce13cc4c50120bfd4bf422
Raiymbek Yeslyam <yeslyamr@gmail.com>
2024-12-31 15:34:43 +0500

    feat: parsing/validating query string parameters

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,6 +12,8 @@ func (app *application) routes() *mux.Router {
 	router.NotFoundHandler = http.HandlerFunc(app.notFoundResponse)
 
 	router.HandleFunc("/v1/healthcheck", app.healthcheckHandler).Methods("GET")
+
+	router.HandleFunc("/v1/movies", app.listMoviesHandler).Methods("GET")
 	router.HandleFunc("/v1/movies", app.createMovieHandler).Methods("POST")
 	router.HandleFunc("/v1/movies/{id}", app.showMovieHandler).Methods("GET")
 	router.HandleFunc("/v1/movies/{id}", app.updateMovieHandler).Methods("PATCH")
