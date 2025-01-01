7ac362e073d2eecbcf59c0f5f3ac6bdd174dd224
Raiymbek Yeslyam <yeslyamr@gmail.com>
2025-01-01 14:24:01 +0500

    feat: List sorting

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,9 @@
 package data
 
-import "letsgofurther/internal/validator"
+import (
+	"letsgofurther/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page         int
@@ -17,4 +20,20 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
