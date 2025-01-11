efe85fc9f9a430a0f20ed10936ab7f8b52664b56
Raiymbek Yeslyam <yeslyamr@gmail.com>
2025-01-11 20:15:00 +0500

    feat: Require Activation && Authentication middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,11 +13,14 @@ func (app *application) routes() http.Handler {
 
 	router.HandleFunc("/v1/healthcheck", app.healthcheckHandler).Methods("GET")
 
-	router.HandleFunc("/v1/movies", app.listMoviesHandler).Methods("GET")
-	router.HandleFunc("/v1/movies", app.createMovieHandler).Methods("POST")
-	router.HandleFunc("/v1/movies/{id}", app.showMovieHandler).Methods("GET")
-	router.HandleFunc("/v1/movies/{id}", app.updateMovieHandler).Methods("PATCH")
-	router.HandleFunc("/v1/movies/{id}", app.deleteMovieHandler).Methods("DELETE")
+	moviesSubrouter := router.PathPrefix("/v1/movies").Subrouter()
+
+	moviesSubrouter.HandleFunc("/", app.listMoviesHandler).Methods("GET")
+	moviesSubrouter.HandleFunc("/", app.createMovieHandler).Methods("POST")
+	moviesSubrouter.HandleFunc("/{id}", app.showMovieHandler).Methods("GET")
+	moviesSubrouter.HandleFunc("/{id}", app.updateMovieHandler).Methods("PATCH")
+	moviesSubrouter.HandleFunc("/{id}", app.deleteMovieHandler).Methods("DELETE")
+	moviesSubrouter.Use(app.requireActivatedUser)
 
 	router.HandleFunc("/v1/users", app.registerUserHandler).Methods("POST")
 	router.HandleFunc("/v1/users/activated", app.activateUserHandler).Methods("PUT")
