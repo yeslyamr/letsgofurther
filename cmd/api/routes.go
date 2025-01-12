77cf3552c984f66932077fdcca299d58d4609ac0
Raiymbek Yeslyam <yeslyamr@gmail.com>
2025-01-12 13:34:43 +0500

    feat: enable CORS

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,18 +10,24 @@ func (app *application) routes() http.Handler {
 
 	router.MethodNotAllowedHandler = http.HandlerFunc(app.methodNotAllowedResponse)
 	router.NotFoundHandler = http.HandlerFunc(app.notFoundResponse)
+	router.Methods(http.MethodOptions)
 
-	router.HandleFunc("/v1/healthcheck", app.healthcheckHandler).Methods("GET")
+	router.HandleFunc("/v1/healthcheck", app.healthcheckHandler).Methods(http.MethodGet)
 
-	router.Handle("/v1/movies", app.requirePermission("movies:read", app.listMoviesHandler)).Methods("GET")
-	router.Handle("/v1/movies", app.requirePermission("movies:write", app.createMovieHandler)).Methods("POST")
-	router.Handle("/v1/movies/{id}", app.requirePermission("movies:read", app.showMovieHandler)).Methods("GET")
-	router.Handle("/v1/movies/{id}", app.requirePermission("movies:write", app.updateMovieHandler)).Methods("PATCH")
-	router.Handle("/v1/movies/{id}", app.requirePermission("movies:write", app.deleteMovieHandler)).Methods("DELETE")
+	router.Handle("/v1/movies", app.requirePermission("movies:read", app.listMoviesHandler)).Methods(http.MethodGet)
+	router.Handle("/v1/movies", app.requirePermission("movies:write", app.createMovieHandler)).Methods(http.MethodPost)
+	router.Handle("/v1/movies/{id}", app.requirePermission("movies:read", app.showMovieHandler)).Methods(http.MethodGet)
+	router.Handle("/v1/movies/{id}", app.requirePermission("movies:write", app.updateMovieHandler)).Methods(http.MethodPatch)
+	router.Handle("/v1/movies/{id}", app.requirePermission("movies:write", app.deleteMovieHandler)).Methods(http.MethodDelete)
 
-	router.HandleFunc("/v1/users", app.registerUserHandler).Methods("POST")
-	router.HandleFunc("/v1/users/activated", app.activateUserHandler).Methods("PUT")
-	router.HandleFunc("/v1/users/authentication", app.createAuthenticationTokenHandler).Methods("POST")
+	router.HandleFunc("/v1/users", app.registerUserHandler).Methods(http.MethodPost)
+	router.HandleFunc("/v1/users/activated", app.activateUserHandler).Methods(http.MethodPut)
+	router.HandleFunc("/v1/users/authentication", app.createAuthenticationTokenHandler).Methods(http.MethodPost)
 
-	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
+	router.Use(app.recoverPanic)
+	router.Use(app.enableCORS)
+	router.Use(app.rateLimit)
+	router.Use(app.authenticate)
+
+	return router
 }
